7e79b03f34166789131bb325c21c9fe7b654b241
DjBainMoussant <antaki.vincent@gmail.com>
2014-11-30 17:07:39 -0500

    splay devrait marcher, pas encore testé. leger debug du programme fait

--- tp2.scm
+++ tp2.scm
@@ -21,25 +21,73 @@
 
 (define node-key cadr)
 (define node-definition caddr)
-(define node-lchild node car)
-(define node-rchild node cadddr)
+(define node-lchild car)
+(define node-rchild cadddr)
 (define (node-create key definition lchild rchild) (list lchild key definition rchild))
 (define (node-reconstruct x lchild rchild) (list lchild (node-key x) (node-definition x) rchild))
 
-(define node-insert 
-        (lambda (root node) 
-                (..........)))
+;(define node-insert 
+;        (lambda (root node) 
+;                (..........)))
 
-(define node-remove ....)
-(define node-replace ....)
+;(define node-remove ....)
                 
 ;(define (node-delete root node)
 ;	 ((let (node node-find(key)))
 ;     ...........))
 
-(define node-splay
-        (lambda (root node)
-                ()))
+; Évalue la profondeur et
+(define node-splay-tree
+        (lambda (root to-splay)
+                (let ((depth (node-depth root (node-key to-splay))))
+                    (cond ((equal? depth 1) to-splay)
+                          ((even? depth) (node-splay '() root (node-splay-tree (get-next-node root to-splay))))
+                          ((odd? depth) (node-splay-repeat root to-splay (/ (- depth 1) 2)))
+                          (else #f)))))
+
+                    
+                    
+(define get-next-node 
+        (lambda (actual final-key)
+                (let ((cmp (if (null? root) #f (compare final-key (node-key actual)))))
+  (cond 
+    ((equal? cmp 'right) (node-rchild root))
+    ((equal? cmp 'left) (node-lchild root)) 
+    (else #f)))))
+;fonction iterative pour les splays zig-zig et zig-zag. On suppose que le sous-arbre qui a g comme noeud a une profondeur de 2*repeat 
+(define node-repeat-splay
+        (lambda (g splay-key repeat)
+                (let ((p (get-next-node g splay-key))
+                      (x (get-next-node p splay-key)))
+                    (if (equal? 1 repeat)
+                        (node-splay g p x)
+                        (node-splay g p (node-repeat-splay x splay-key (- 1 repeat)))))))
+                        
+; g (s'il existe) est le noeud parent de p.
+; p est le noeud parent de x.
+; p et x doivent exister sinon -> plante.
+(define node-splay 
+        (lambda (g p x)
+                ;assign
+                (let ((x-left (equal? (node-lchild p) x)))
+                      ;(p-left (equal? (node-lchild g) p)))
+                ;then
+                    (if (null? g)
+                        ; si true cas de zig zig
+                        (if (x-left)
+                            (zig-left p x)
+                            (zig-right p x))
+                        (let ((p-left (equal? (node-lchild g) p)))
+                             (if (equal? p-left x-left)
+                                 ; Si true on est dans un cas de zig-zig
+                                 (if (x-left) 
+                                     (zig-zig-left g p x)
+                                     (zig-zig-right g p x))
+                                 ; Si false on est dans un cas de zig-zag
+                                 (if (x-left)
+                                     (zig-zag-left g p x)
+                                     (zig-zag-right g p x))))))))
+                    
                 
 ;
 ;Opération zig. p etait root -> c est root
@@ -69,6 +117,17 @@
                                 (node-reconstruct p (node-rchild x)  
                                                     (node-reconstruc g (node-rchild p)
                                                                        (node-rchild g))))))
+                                                                       
+(define (node-depth root key)
+        (if (null? root) 
+             (0)
+             (let ((cmp (compare key (node-key root))))
+                  (cond 
+                        ((equal? cmp 'youfoundme) 1)
+                        ((equal? cmp 'right) (+ 1 (node-depth (node-rchild root) key))
+                        ((equal? cmp 'left) (+ 1 (node-depth (node-lchild root) key)) 
+                         (else #f)))))))                                                                       
+                                                                       
 #|                                
 (define zig-zag-left
         (lambda (g p x)
@@ -80,31 +139,29 @@
                 
   |#              
 (define (node-find root key) 
- ((if (null? root) 
-       (let cmp -1)
-       (let cmp (compare key (node-key root))))
+ (let ((cmp (if (null? root) #f (compare key (node-key root)))))
   (cond 
-    ((equal? cmp 0) root )
-    ((equal? cmp 1) (node-find (node-rchild root) key))
-    ((equal? cmp 2) (node-find (node-lchild root) key )) 
+    ((equal? cmp 'youfoundme) root )
+    ((equal? cmp 'right) (node-find (node-rchild root) key))
+    ((equal? cmp 'left) (node-find (node-lchild root) key )) 
     (else #f))))
-
+    
 ;; prends en input une liste de terme a concatener
 ;; retourne une liste de definition concatener
 (define (make-concatdefinition dict lst)
         (foldr 
             (lambda (x y) 
-                    (let (d (node-find dict x))
+                    (let ((d (node-find dict x)))
                      (if (and d y) 
                          (cons (node-definition d) y)
                           #f)))                         
             '() lst))
-    
+; Pour utilisation consquente dans le programme, appeler avec str1=clé recherchée et str2 noeud actuel
 (define (compare str1 str2)
  (cond
-  ((string< str1 str2) 2)
-  ((string> str1 str2) 1)
-  ((string= str1 str2) 0)
+  ((string< str1 str2) 'left)
+  ((string> str1 str2) 'right)
+  ((string= str1 str2) 'youfoundme)
   (else -1)))
     
 ;; y'a clairement des endroits ou il va falloir utiliser ca. on la que trop vu souvent en cours    
@@ -131,7 +188,7 @@
   
 (define (eval-expr expr) 
         (if (member '= expr) 
-			(let (expr (string-split expr '=))
+			(let ((expr (string-split expr '=)))
 				(if (null? (cdar expr))
 					(cons '- (car expr));;;retrait du mot expr
 					(append (list '= (car expr))
@@ -145,7 +202,7 @@
 (define traiter
   (lambda (expr dict)
    ;;;evaluer l'expression
-   (let (result (eval-expr expr))
+   (let ((result (eval-expr expr)))
 			(cond((equal? (car result) '-);;;result est de la forme ('- key) et il faut delete le mot key
 				  (...);;;appel à node-delete avec (cdr result)?
 				 )
@@ -173,8 +230,6 @@
           dict)))
 
 ;;;----------------------------------------------------------------------------
-((let (x 1))
- ())
 ;;; Ne pas modifier cette section.
 
 (define go
